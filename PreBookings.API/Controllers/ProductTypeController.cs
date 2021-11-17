e61f1426db847b4b531ab85159e40515c108b0ee
Phaneendra Varanasi <varanasikrishnaphaneendra@gmail.com>
2021-11-17 11:24:19 +0530

    product type controller

---
+++ PreBookings.API/Controllers/ProductTypeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreBookings.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductTypeController : ControllerBase
+    {
+        
+        [HttpGet]
+        public ActionResult CreateData()
+        {
+            return Ok();
+        }
+    }
+}
